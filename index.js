1f59a87b2ac0f3612e4de7002d357d24c0589215
Sarah Dean <sarah.schmidtdean@gmail.com>
2023-04-15 21:50:34 -0500

    reducer lab

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//totalBatteries variable - holds the sum of all the battery amounts in the batteryBatches array
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+// const initialValue = 0;
+
+// const sumWithInitial = array1.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue
+// );
+
+let totalBatteries = batteryBatches.reduce((accumulator, currentNumber) => {const initialValue = 0;
+    const batteryAmount = initialValue + currentNumber;
+return accumulator + batteryAmount})
+
+console.log(totalBatteries);
